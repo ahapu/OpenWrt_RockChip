1e2973a862732ed4bdc839a327280509763f4528
ahapuWang <55306019+ahapu@users.noreply.github.com>
2025-01-28 10:52:14 +0800

    Update lede.sh

--- scripts/lede.sh
+++ scripts/lede.sh
@@ -25,15 +25,15 @@ rm -rf openwrt-package/verysync
 rm -rf openwrt-package/luci-app-verysync
 
 # apppppppp
-git clone --depth=1 https://github.com/DHDAXCW/dhdaxcw-app
+#git clone --depth=1 https://github.com/DHDAXCW/dhdaxcw-app
 
 # Add luci-app-ssr-plus
-git clone --depth=1 https://github.com/fw876/helloworld
+#git clone --depth=1 https://github.com/fw876/helloworld
 
 # Add luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # Add OpenClash
 git clone --depth=1 https://github.com/vernesong/OpenClash
@@ -48,11 +48,11 @@ cp -f $GITHUB_WORKSPACE/data/bg1.jpg luci-theme-argon/htdocs/luci-static/argon/i
 git clone https://github.com/DHDAXCW/theme
 
 # Add luci-app-lucky
-git clone --depth=1 -b 2.13.4 https://github.com/DHDAXCW/luci-app-lucky
+#git clone --depth=1 -b 2.13.4 https://github.com/DHDAXCW/luci-app-lucky
 rm -rf ../../customfeeds/packages/net/lucky
 
 # alist
-git clone -b lua --depth=1 https://github.com/sbwml/luci-app-alist
+#git clone -b lua --depth=1 https://github.com/sbwml/luci-app-alist
 
 # Add OpenAppFilter
 git clone --depth=1 https://github.com/destan19/OpenAppFilter
@@ -60,7 +60,7 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter
 # qbittorrent
 rm -rf ../../customfeeds/packages/net/qBittorrent
 rm -rf ../../customfeeds/packages/libs/rblibtorrent
-git clone --depth=1 https://github.com/sbwml/luci-app-qbittorrent
+#git clone --depth=1 https://github.com/sbwml/luci-app-qbittorrent
 rm -rf luci-app-qbittorrent/luci-app-qbittorrent
 
 popd
@@ -85,4 +85,4 @@ rm -rf package/feeds/packages/adguardhome
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
